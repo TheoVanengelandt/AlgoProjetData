e7e6f028f37cbf349c52edc8a2ab7846205e5512
Theo <theo@ideine.fr>
2019-07-03 15:36:43 +0200

    correct file error display

--- tsp/main.py
+++ tsp/main.py
@@ -5,8 +5,7 @@ import json
 
 routeManager = RouteManager()
 
-print('Nom du fichier à étudier ? (ex: nomFichier.json)')
-fileName = input()
+fileName = input('Nom du fichier à étudier ? (ex: nomFichier.json) ')
 dataFileName = str('../data/' + fileName)
 
 try:
@@ -17,26 +16,29 @@ try:
 
             RouteManager.addDustbin(Dustbin(city['pos'][0], city['pos'][1]))
 
+    evolutionNumber = int(input("Combien d'évolution ? (default = 40) ") or  "40")
+
+    pop = Population(RouteManager.numberOfDustbins(), True)
+    print('\nInitial distance: ' + str(pop.getFittest().getDistance()))
+    yaxis = []
+    xaxis = []
+    # Nombre d'evolution que va réaliser l'algorithme
+    for i in range(evolutionNumber):
+        pop = GA.evolvePopulation(pop)
+        fittest = pop.getFittest().getDistance()
+        yaxis.append(fittest)
+        xaxis.append(i)
+        if fittest < 1050:
+            break
+    
+    print('Final distance: ' + str(fittest))
+    print('Final Route: ' + pop.getFittest().toString())
+    
+    fig = plt.figure()
+    #plt.ylim(0, 80000)
+    plt.plot(xaxis, yaxis, 'r-')
+    plt.show()
+
 except (OSError, IOError) as e:
     print(str(e) + '!')
     input("Press Enter to close...")
-
-
-pop = Population(RouteManager.numberOfDustbins(), True)
-print('Initial distance: ' + str(pop.getFittest().getDistance()))
-yaxis = []
-xaxis = []
-for i in range(RouteManager.numberOfDustbins()):
-    pop = GA.evolvePopulation(pop)
-    fittest = pop.getFittest().getDistance()
-    yaxis.append(fittest)
-    xaxis.append(i)
-    if fittest < 1050:
-        break
-print('Final distance: ' + str(fittest))
-print('Final Route: ' + pop.getFittest().toString())
-
-fig = plt.figure()
-#plt.ylim(0, 80000)
-plt.plot(xaxis, yaxis, 'r-')
-plt.show()
